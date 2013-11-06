063be19a09ce62980ee11bfed7dc959ac248df34
Nick Willett-Jeffries & Kimberly Narine <nick+kimberly@appacademy.io>
2013-11-06 15:25:54 -0500

    Randomly generate string for short url

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,4 +1,5 @@
 class ShortenedUrl < ActiveRecord::Base
+  attr_accessible :short_url, :long_url, :user_id
 
   validates :short_url, :long_url, :user_id, :presence => true
   validates :short_url, :uniqueness => true
@@ -9,3 +10,24 @@ class ShortenedUrl < ActiveRecord::Base
     foreign_key: :user_id,
     primary_key: :id
   )
+
+  def self.random_code
+    random_code = SecureRandom::urlsafe_base64
+
+    until ShortenedUrl.find_by_short_url(random_code).nil?
+      random_code = SecureRandom::urlsafe_base64
+    end
+
+    random_code
+  end
+
+  def self.create_for_user_and_long_url!(user, long_url)
+    ShortenedUrl.create(
+    short_url: ShortenedUrl.random_code,
+    long_url: long_url,
+    user_id: user.id
+  )
+  end
+
+
+end
