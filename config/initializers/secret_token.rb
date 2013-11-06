f72a196a3f941f647dcef9290123cc1dafea595a
Nick Willett-Jeffries & Kimberly Narine <nick+kimberly@appacademy.io>
2013-11-06 11:44:06 -0500

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = 'd1b6a638bff453d958403934da50823be3311b6812610a4e94d90913cffad2de3d8818c33bf6295edf242649c5112a8fc64829f27558b880b9b07313337a4548'
